48083833d9955259f5c956f860474d43cb1b1695
DiegoLopezAroca <diego.lopez.a@opendeusto.es>
2025-10-21 13:42:14 +0200

    urls

--- proj_iw/appProjectDjango/admin.py
+++ proj_iw/appProjectDjango/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Socio, Eventos, Cuoutas, Merchandising
 
 # Register your models here.
+admin.site.register(Socio)
+admin.site.register(Eventos)
+admin.site.register(Cuoutas)
+admin.site.register(Merchandising)
---
+++ proj_iw/appProjectDjango/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('registros/', views.registros, name='registros'),
+    path('eventos/', views.eventos, name='eventos'),
+    path('eventos/<int:evento_id>/', views.detalle_evento, name='detalle_evento'),
+    path('eventos/<int:evento_id>/socios/', views.socios_evento, name='socios_evento'),
+    path('cuotas/', views.cuotas, name='cuotas'),
+    path('cuotas/<int:cuota_id>/', views.detalle_cuota, name='detalle_cuota'),
+    path('socios/', views.socios, name='socios'),
+    path('socios/<int:socio_id>/', views.detalle_socio, name='detalle_socio'),
+    path('merchandising/', views.merchandising, name='merchandising'),
+    path('merchandising/<int:producto_id>/', views.detalle_producto, name='detalle_producto')
+]
