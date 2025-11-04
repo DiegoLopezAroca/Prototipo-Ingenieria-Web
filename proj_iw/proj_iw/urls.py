39a083f7fa84a1d10aa95024fc2ad53f65b7cac5
Mirenlepee <miren.lepee@opendeusto.es>
2025-11-04 13:37:16 +0100

    Añadir admin + corregir el acceso (url)

--- proj_iw/proj_iw/urls.py
+++ proj_iw/proj_iw/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('appProjectDjango/', include('appProjectDjango.urls')),
+    #path('appProjectDjango/', include('appProjectDjango.urls')),
+    path('', include('appProjectDjango.urls')),
 ]
