bdaa9687f326545ef0d231a05d05d0a8d48b93c6
Alba <145106923+AlbaDelgadoO@users.noreply.github.com>
2025-11-25 12:36:56 +0100

    creacion po

--- proj_iw/proj_iw/urls.py
+++ proj_iw/proj_iw/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    #path('appProjectDjango/', include('appProjectDjango.urls')),
-    path('', include('appProjectDjango.urls')),
+    # Ruta para cambiar idioma (necesaria para {% url 'set_language' %})
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
+
+# URLs traducibles
+urlpatterns += i18n_patterns(
+    path('admin/', admin.site.urls),
+    path('', include('appProjectDjango.urls')),
+)
+
